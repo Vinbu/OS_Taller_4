10f6250a6a0771b4186f09599c9150dd50bce794
AlanSilvaaa <asilvar4@correo.uss.cl>
2025-03-25 17:52:13 -0300

    format document

--- main.cpp
+++ main.cpp
@@ -1,31 +1,29 @@
-#include <opencv2/opencv.hpp>
 #include <iostream>
+#include <opencv2/opencv.hpp>
 
 using namespace cv;
 using namespace std;
 
-int main(int argc, char** argv)
-{
- // Read the image file
- Mat image = imread("../capi.jpg");
+int main(int argc, char **argv) {
+    // Read the image file
+    Mat image = imread("../capi.jpg");
 
- // Check for failure
- if (image.empty()) 
- {
-  cout << "Could not open or find the image" << endl;
-  cin.get(); //wait for any key press
-  return -1;
- }
+    // Check for failure
+    if (image.empty()) {
+        cout << "Could not open or find the image" << endl;
+        cin.get(); // wait for any key press
+        return -1;
+    }
 
- String windowName = "The legendary capibara"; //Name of the window
+    String windowName = "The legendary capibara"; // Name of the window
 
- namedWindow(windowName); // Create a window
+    namedWindow(windowName); // Create a window
 
- imshow(windowName, image); // Show our image inside the created window.
+    imshow(windowName, image); // Show our image inside the created window.
 
- waitKey(0); // Wait for any keystroke in the window
+    waitKey(0); // Wait for any keystroke in the window
 
- destroyWindow(windowName); //destroy the created window
+    destroyWindow(windowName); // destroy the created window
 
- return 0;
+    return 0;
 }
