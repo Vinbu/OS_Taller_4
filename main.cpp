d79172240f67f4749466dfb8a9cd298dadd4e841
Vinbu <vincent.14.damian@gmail.com>
2025-03-30 22:33:33 -0300

    Parser added

--- main.cpp
+++ main.cpp
@@ -3,11 +3,11 @@
 #include <opencv2/imgcodecs.hpp>
 #include <opencv2/opencv.hpp>
 #include <filesystem>
+#include <unistd.h>
 
 using namespace cv;
 using namespace std;
 
-const int NUM_THREADS = 5;
 
 // Each threadd will have its own data structure to hold the image and output path. 
 struct ThreadData {
@@ -41,70 +41,120 @@ void* thread_convert(void* arg) {
 }
 
 int main(int argc, char **argv) {
-    // Load image filenames.
-    vector<cv::String> fn;
-    glob("../images/*", fn, false);
-    if (fn.empty()) {
-        cout << "No images found in the specified directory." << endl;
-        return -1;
-    }
+    int c;
+    bool gFlag = false, fFlag = false;
+    std::string input, output;
+    int num_threads = 0;
 
-    // Load images from the filenames.
-    vector<Mat> images;
-    for (size_t i = 0; i < fn.size(); i++) {
-        Mat img = imread(fn[i]);
-        if (img.empty()) {
-            cout << "Could not open or find the image: " << fn[i] << endl;
-            continue;
+    while ((c = getopt(argc, argv, "gfi:o:n:")) != -1) {
+        switch (c) {
+            case 'g':
+                if (fFlag) {
+                    std::cerr << "Error: No se puede usar -g junto con -f" << std::endl;
+                    return 1;
+                }
+                gFlag = true;
+                break;
+
+            case 'f':
+                if (gFlag) {
+                    std::cerr << "Error: No se puede usar -f junto con -g" << std::endl;
+                    return 1;
+                }
+                fFlag = true;
+                break;
+
+            case 'i':
+                input = optarg;
+                break;
+
+            case 'o':
+                output = optarg;
+                break;
+
+            case 'n':
+                num_threads = std::stoi(optarg);
+                break;
+
+            default:
+                std::cerr << "Uso: " << argv[0] << " [-g | -f] -i input -o output -n threads" << std::endl;
+                return 1;
         }
-        images.push_back(img);
     }
 
-    // Create output directory.
-    std::string outDir = "../out";
-    try {
-        if (std::filesystem::create_directory(outDir)) {
-            cout << "Directory created successfully: " << outDir << endl;
-        } else {
-            cout << "Directory already exists: " << outDir << endl;
+    if (gFlag) {
+        // Load image filenames.
+        vector<cv::String> fn;
+        cout << "Searching in directory: " << input << endl;
+        glob(input, fn, false);
+        if (fn.empty()) {
+            cout << "No images found in the specified directory." << endl;
+            return -1;
         }
-    } catch (const std::filesystem::filesystem_error& e) {
-        cerr << "Error creating directory: " << e.what() << endl;
-        return -1;
-    }
 
-    // Process images in batches of NUM_THREADS concurrently.
-    size_t total = images.size();
-    for (size_t i = 0; i < total; i += NUM_THREADS) {
-        pthread_t threads[NUM_THREADS];
-        int threadsCreated = 0;
-        
-        // Create up to NUM_THREADS threads if there are images available.
-        for (int j = 0; j < NUM_THREADS && (i + j) < total; j++) {
-            // Prepare thread data.
-            ThreadData* data = new ThreadData;
-            data->image = images[i + j];  // Copy the image.
-            data->outputPath = outDir + "/gray_" + std::to_string(i + j) + ".jpg";
-            data->threadIndex = i + j; // Assign a unique thread index.
-            
-            cout << "Main thread: Creating thread " << data->threadIndex << endl;
-            // Create the thread.
-            int rc = pthread_create(&threads[j], nullptr, thread_convert, (void*)data);
-            if (rc) {
-                cerr << "Error: unable to create thread, " << rc << endl;
-                delete data;  // Clean up if thread creation fails.
+        // Load images from the filenames.
+        vector<Mat> images;
+        for (size_t i = 0; i < fn.size(); i++) {
+            Mat img = imread(fn[i]);
+            if (img.empty()) {
+                cout << "Could not open or find the image: " << fn[i] << endl;
                 continue;
             }
-            threadsCreated++;
+            images.push_back(img);
         }
-        
-        // Wait for the created threads to finish.
-        for (int j = 0; j < threadsCreated; j++) {
-            pthread_join(threads[j], nullptr);
-            cout << "Main thread: Joined thread " << (i + j) << endl;
+
+        // Create output directory.
+        std::string outDir = output;
+        try {
+            if (std::filesystem::create_directory(outDir)) {
+                cout << "Directory created successfully: " << outDir << endl;
+            } else {
+                cout << "Directory already exists: " << outDir << endl;
+            }
+        } catch (const std::filesystem::filesystem_error& e) {
+            cerr << "Error creating directory: " << e.what() << endl;
+            return -1;
         }
+
+        // Process images in batches of threads concurrently.
+        size_t total = images.size();
+        for (size_t i = 0; i < total; i += num_threads) {
+            pthread_t threads[num_threads];
+            int threadsCreated = 0;
+            
+            // Create up to NUM_THREADS threads if there are images available.
+            for (int j = 0; j < num_threads && (i + j) < total; j++) {
+                // Prepare thread data.
+                ThreadData* data = new ThreadData;
+                data->image = images[i + j];  // Copy the image.
+                data->outputPath = outDir + "/gray_" + std::to_string(i + j) + ".jpg";
+                data->threadIndex = i + j; // Assign a unique thread index.
+                
+                cout << "Main thread: Creating thread " << data->threadIndex << endl;
+                // Create the thread.
+                int rc = pthread_create(&threads[j], nullptr, thread_convert, (void*)data);
+                if (rc) {
+                    cerr << "Error: unable to create thread, " << rc << endl;
+                    delete data;  // Clean up if thread creation fails.
+                    continue;
+                }
+                threadsCreated++;
+            }
+            
+            // Wait for the created threads to finish.
+            for (int j = 0; j < threadsCreated; j++) {
+                pthread_join(threads[j], nullptr);
+                cout << "Main thread: Joined thread " << (i + j) << endl;
+            }
+        }
+
+        cout << "All images processed and saved." << endl;
+        return 0;
     }
 
-    cout << "All images processed and saved." << endl;
+    if (fFlag) {
+        // Future Implementation
+        return 0;
+    }
     return 0;
 }
